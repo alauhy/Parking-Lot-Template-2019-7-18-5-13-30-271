6f02d45df6809f3af09ea257e323fde4a1b406bb
alauhy <751961605@qq.com>
2019-07-18 17:11:16 +0800

    [LXY]:Story1 AC4 Repository implement

--- src/test/java/com/thoughtworks/parking_lot/repository/ParikinglotRepositoryTest.java
+++ src/test/java/com/thoughtworks/parking_lot/repository/ParikinglotRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -18,7 +21,7 @@ public class ParikinglotRepositoryTest {
     private ParkingLotRepository parkingLotRepository;
 
     @Test
-    void should_add_parkinglot_and_find_parkinglot(){
+    void should_add_parkinglot_and_find_parkinglot() {
         ParkingLot parkingLot = new ParkingLot();
         parkingLot.setName("OOCL parking lot");
         parkingLot.setCapacity(10);
@@ -33,7 +36,7 @@ public class ParikinglotRepositoryTest {
     }
 
     @Test
-    void should_delete_parkinglot_by_name(){
+    void should_delete_parkinglot_by_name() {
         ParkingLot parkingLot = new ParkingLot();
         parkingLot.setName("OOCL parking lot");
         parkingLot.setCapacity(10);
@@ -53,7 +56,25 @@ public class ParikinglotRepositoryTest {
         assertThat(parkingLots.get(0).getCapacity()).isEqualTo(110);
     }
 
+    @Test
+    void should_page_parkinglot() {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setName("OOCL parking lot");
+        parkingLot.setCapacity(10);
+        parkingLot.setAddress("ZHA");
+        ParkingLot parkingLot1 = new ParkingLot();
+        parkingLot1.setName("CS parking lot");
+        parkingLot1.setCapacity(110);
+        parkingLot1.setAddress("ZHA");
+        parkingLotRepository.save(parkingLot);
+        parkingLotRepository.save(parkingLot1);
+        Pageable pageable = new PageRequest(1, 1);
 
+        Page<ParkingLot> parkingLots = parkingLotRepository.findAll(pageable);
+        assertThat(parkingLots.getContent().get(0).getName()).isEqualTo("CS parking lot");
+        assertThat(parkingLots.getContent().get(0).getAddress()).isEqualTo("ZHA");
+        assertThat(parkingLots.getContent().get(0).getCapacity()).isEqualTo(110);
+    }
 
 
 }
